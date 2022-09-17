caa09e1e8ce1f3639897d2594791eba440ef9197
Andrés Olivé <andresolive@gmail.com>
2022-09-17 23:10:04 +0200

    build: Home screen

---
+++ screens/Home.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+const Home = () => {
+  return <Text>Home component!</Text>;
+};
+
+export default Home;
