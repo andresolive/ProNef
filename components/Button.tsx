6fb0a786b655fa36abe54ab02b62362ed7f662ba
Andrés Olivé <andresolive@gmail.com>
2022-09-18 13:02:03 +0200

    feat: circle button ready

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,45 @@
-import { View, Text } from 'react-native';
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  ImageSourcePropType,
+} from 'react-native';
+import { COLORS, SHADOWS, SIZES } from '../constants';
 
 interface CircleButtonProps {
-  imgUrl: string;
+  imgUrl: ImageSourcePropType;
+  handlePress?: any;
   right: number;
   top: number;
 }
 
-export const CircleButton = ({ imgUrl, right, top }: CircleButtonProps) => {
+export const CircleButton = ({
+  imgUrl,
+  handlePress,
+  ...props
+}: CircleButtonProps) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: 'absolute',
+        borderRadius: SIZES.extraLarge,
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...SHADOWS.light,
+        ...props,
+        // onPress = { handlePress },
+      }}
+    >
+      <Image
+        source={imgUrl}
+        resizeMode='contain'
+        style={{ width: 24, height: 24 }}
+      />
+    </TouchableOpacity>
   );
 };
 
