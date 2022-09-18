9467b9e0233747461c1097873593f16f75150666
Andrés Olivé <andresolive@gmail.com>
2022-09-18 18:18:44 +0200

    build: added search handler

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -2,7 +2,7 @@ import { View, Text, Image, TextInput } from 'react-native';
 
 import { COLORS, FONTS, SIZES, assets } from '../constants';
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }: any) => {
   return (
     <View
       style={{
@@ -85,7 +85,7 @@ const HomeHeader = () => {
           <TextInput
             placeholder='Search NFTs'
             style={{ flex: 1 }}
-            onChangeText={() => {}}
+            onChangeText={onSearch}
           />
         </View>
       </View>
